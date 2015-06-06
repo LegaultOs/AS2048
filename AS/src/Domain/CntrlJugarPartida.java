330a015729fe386a98f243ce0141efebd0eb9435
WANG YANG YE <wangyang.ye@est.fib.upc.edu>
2015-06-06 19:13:01 +0200

    estrategia

--- AS/src/Domain/CntrlJugarPartida.java
+++ AS/src/Domain/CntrlJugarPartida.java
@@ -7,58 +7,42 @@ import Domain.DTO.InfoPartidaDTO;
 import Domain.DTO.JugadorDTO;
 import Domain.DTO.MovimentDTO;
 
-
-
 public class CntrlJugarPartida {
 
 	private Jugador jugador;
 	private Partida p;
 	
-	
-	
 	public InfoPartidaDTO crearPartida()
-	{
-		ArrayList<CasellaDTO> info = new ArrayList<CasellaDTO>();
-		
-		InfoPartidaDTO result = new InfoPartidaDTO();
-		
+	{	
 		Joc2048 joc =Joc2048.getInstance();
-		
 		int id=joc.getIdPartida();
-		
 		id++;
-		/*esto de aqui nose si tenemos que hacerlo aqui o dentro de assignaEstrategiaOrdenacioPunts, 
-		 * por que en los diagramas me he encontrado las 2 cosas .
-		 * de momento se queda aquí*/
+		joc.setId(id);
+
 		EstrategiaOrdenacioFactory eof= EstrategiaOrdenacioFactory.getInstance();
-		
 		EstrategiaOrdenacio op = eof.creaEstrategiaOrdenacioPunts();
-		
 		joc.assignaEstrategiaOrdenacioPunts(op);
-		/*----------------------------------------------*/
-		p = new Partida(id);
 		
+		p = new Partida(id);
 		p.inicializar();
 		
+		ArrayList<CasellaDTO> info = new ArrayList<CasellaDTO>();
 		info = p.casellesAmbNumero();
 		
 		int pi = p.getPuntuacio();
-		
 		int mp = jugador.getMillorPuntuacio();
 		
-		
+		InfoPartidaDTO result = new InfoPartidaDTO();
 		result.setInfo(info);
 		result.setMillorPuntuacio(mp);
 		result.setPuntuacio(pi);
-		
 		return result;
-			
 	}
 	
 	
 	public void  ferAutenticacio(String userN, String passwd)
 	{
-		//aqui se creará el jugador y tal y cual
+		//aqui se crear? el jugador y tal y cual
 		
 		
 	}
--- AS/src/Domain/Joc2048.java
+++ AS/src/Domain/Joc2048.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 import Domain.DTO.JugadorDTO;
 
 public class Joc2048 {
-
+	
 	private static Joc2048 instance;
 	private int idPartida;	
-	private EstrategiaOrdenacio strat;
+	private EstrategiaOrdenacio estrategia;
+	
 	private Joc2048()
-	{
-		
-		
-	}
+	{	}
 	
 	public static Joc2048 getInstance()
 	{
@@ -21,41 +19,28 @@ public class Joc2048 {
 		if(instance==null)
 		{
 			instance= new Joc2048();
-			
-			
 		}
 		
 		return instance;
-		
-		
 	}
 
 	public int getIdPartida() {
 		return idPartida;
 	}
 
-	public void setIdPartida(int idPartida) {
+	public void setId(int idPartida) {
 		this.idPartida = idPartida;
 	}
-	
 	
 	public void assignaEstrategiaOrdenacioPunts(EstrategiaOrdenacio op)
 	{
 		if(op.getClass().equals(EstrategiaOrdenacioPunts.class))
-		{
-			
-			strat=op;
-		}
-		
-		
+			estrategia=op;
 	}
 	
 	public ArrayList<JugadorDTO> obtenirRanquing(ArrayList<Jugador>jug ,int cont)
 	{
-		
-		return strat.getRanquing(jug, cont);
-		
+		return estrategia.getRanquing(jug, cont);
 	}
-	
-	
+		
 }

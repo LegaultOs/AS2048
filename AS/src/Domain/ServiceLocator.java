1f4e98caeaa3e7b2550a52c2588180cdd4aed1d6
legaultos <oscaracso90@gmail.com>
2015-06-11 13:55:32 +0200

    Añadida libreria para enviar mails

--- AS/src/Domain/ServiceLocator.java
+++ AS/src/Domain/ServiceLocator.java
@@ -1,30 +1,43 @@
 package Domain;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 public class ServiceLocator {
-	
+
 	private static ServiceLocator instance;
+	private Map services = new HashMap();
 	
 	
-	private ServiceLocator(){}
-	
-	
-	public static ServiceLocator getInstance()
-	{
-		if(instance==null)
-		{
-			
+
+	private ServiceLocator(){
+		
+		loadService("ServeiMissatgeria", new ServeiMissatgeria());
+		
+	}
+
+	public static ServiceLocator getInstance(){
+		if (instance == null) {
+
 			instance = new ServiceLocator();
 		}
-		
+
 		return instance;
 	}
 	
-	public Object find (String nom)
-	{
-		//entiendo que tenemos que devolver un objeto y luego castearlo a lo que estuvieramos buscando no?
-		
-		//TODO
-		
-		return null;
-	}
+	
+	public static Object find(String key){
+	      return instance.services.get(key);
+	  }
+	private void loadService (String key, Object service) {
+		instance.services.put(key, service);
+	  }
+
+	
 }
